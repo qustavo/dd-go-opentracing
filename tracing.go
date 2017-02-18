05a8dc5bf32f6feb9910f7bc4e93ac4c1a6e6bc4
Gustavo Chain <gchain@gmail.com>
2017-02-18 11:15:37 -0300

    Initial implementation

---
+++ tracing.go
@@ -0,0 +1,140 @@
+package ddtracer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/DataDog/dd-trace-go/tracer"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
+)
+
+type Tracer struct {
+	*tracer.Tracer
+	srv string
+}
+
+func NewTracer() opentracing.Tracer {
+	return &Tracer{
+		Tracer: tracer.NewTracer(),
+	}
+}
+
+func NewTracerTransport(tr tracer.Transport) opentracing.Tracer {
+	return &Tracer{
+		Tracer: tracer.NewTracerTransport(tr),
+	}
+}
+
+func (t *Tracer) StartSpan(op string, opts ...opentracing.StartSpanOption) opentracing.Span {
+	sso := &opentracing.StartSpanOptions{}
+	for _, o := range opts {
+		o.Apply(sso)
+	}
+
+	return t.startSpanWithOptions(op, sso)
+}
+
+func (t *Tracer) startSpanWithOptions(op string, opts *opentracing.StartSpanOptions) opentracing.Span {
+	var span *tracer.Span
+	for _, ref := range opts.References {
+		if ref.Type == opentracing.ChildOfRef {
+			if p, ok := ref.ReferencedContext.(*SpanContext); ok {
+				span = tracer.NewChildSpanFromContext(op, p.ctx)
+			}
+		}
+	}
+
+	if span == nil {
+		span = t.NewRootSpan(op, t.srv, "")
+	}
+
+	return &Span{span}
+
+}
+
+func (t *Tracer) Inject(sm opentracing.SpanContext, format interface{}, carrier interface{}) error {
+	panic("not implemented")
+}
+
+func (t *Tracer) Extract(format interface{}, carrier interface{}) (opentracing.SpanContext, error) {
+	panic("not implemented")
+}
+
+type Span struct {
+	*tracer.Span
+}
+
+func (s *Span) Finish() {
+	s.Span.Finish()
+}
+
+func (s *Span) FinishWithOptions(opts opentracing.FinishOptions) {
+	panic("not implemented")
+}
+
+func (s *Span) Context() opentracing.SpanContext {
+	ctx := s.Span.Context(context.Background())
+	return &SpanContext{ctx}
+}
+
+func (s *Span) SetOperationName(operationName string) opentracing.Span {
+	s.Name = operationName
+	return s
+}
+
+func (s *Span) SetTag(key string, value interface{}) opentracing.Span {
+	panic("not implemented")
+}
+
+func (s *Span) LogFields(fields ...log.Field) {
+	for _, field := range fields {
+		value := field.Value()
+		switch t := value.(type) {
+		case float64:
+			s.SetMetric(field.Key(), t)
+		default:
+			s.SetMeta(field.Key(), fmt.Sprint(value))
+		}
+	}
+}
+
+func (s *Span) LogKV(alternatingKeyValues ...interface{}) {
+	fields, err := log.InterleavedKVToFields(alternatingKeyValues...)
+	if err != nil {
+		return
+	}
+	s.LogFields(fields...)
+}
+
+func (s *Span) SetBaggageItem(restrictedKey string, value string) opentracing.Span {
+	panic("not implemented")
+}
+
+func (s *Span) BaggageItem(restrictedKey string) string {
+	panic("not implemented")
+}
+
+func (s *Span) Tracer() opentracing.Tracer {
+	return s.Tracer()
+}
+
+func (s *Span) LogEvent(event string) {
+	panic("not implemented")
+}
+
+func (s *Span) LogEventWithPayload(event string, payload interface{}) {
+	panic("not implemented")
+}
+
+func (s *Span) Log(data opentracing.LogData) {
+	panic("not implemented")
+}
+
+type SpanContext struct {
+	ctx context.Context
+}
+
+func (ctx *SpanContext) ForeachBaggageItem(handler func(k, v string) bool) {
+	panic("not implemented")
+}
---
+++ tracing_test.go
@@ -0,0 +1,113 @@
+package ddtracer
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/DataDog/dd-trace-go/tracer"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type env struct {
+	reqs []*http.Request
+	ts   *httptest.Server
+	tr   opentracing.Tracer
+}
+
+func newEnv() *env {
+	e := &env{}
+	e.ts = httptest.NewServer(e)
+	url, _ := url.Parse(e.ts.URL)
+	hostPort := strings.Split(url.Host, ":")
+	e.tr = NewTracerTransport(
+		//		tracer.NewTransport("localhost", "8000"),
+		tracer.NewTransport(hostPort[0], hostPort[1]),
+	)
+	log.Printf("url.Host = %+v\n", url.Host)
+	return e
+}
+
+func (e *env) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if strings.Contains(req.URL.String(), "v0.3") {
+		w.WriteHeader(415)
+		return
+	}
+
+	buf, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	newReq, err := http.ReadRequest(bufio.NewReader(bytes.NewBuffer(buf)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	e.reqs = append(e.reqs, newReq)
+}
+
+func (e *env) reset() {
+	e.reqs = make([]*http.Request, 0)
+}
+
+func (e *env) close() {
+	e.reset()
+	e.ts.Close()
+}
+
+func TestSpansParenthood(t *testing.T) {
+	env := newEnv()
+	defer env.close()
+
+	pSpan := env.tr.StartSpan("parent")
+	cSpan := env.tr.StartSpan("child", opentracing.ChildOf(pSpan.Context()))
+	cSpan.Finish()
+	pSpan.Finish()
+
+	err := env.tr.(*Tracer).FlushTraces()
+	require.NoError(t, err)
+
+	var spans [][]*tracer.Span
+	if err := json.NewDecoder(env.reqs[0].Body).Decode(&spans); err != nil {
+		require.NoError(t, err)
+	}
+
+	child := spans[0][0]
+	parent := spans[0][1]
+
+	assert.Equal(t, "child", child.Name)
+	assert.Equal(t, parent.SpanID, child.ParentID)
+	assert.Equal(t, child.TraceID, parent.TraceID)
+}
+
+func TestSpanTags(t *testing.T) {
+	span := NewTracer().StartSpan("test")
+	span.LogKV(
+		"foo", "bar",
+		"key", "val",
+		"int", 123,
+		"metric", 0.1,
+	)
+
+	assert.Equal(t, "bar", span.(*Span).GetMeta("foo"))
+	assert.Equal(t, "val", span.(*Span).GetMeta("key"))
+	assert.Equal(t, "123", span.(*Span).GetMeta("int"))
+	assert.Equal(t, 0.1, span.(*Span).Metrics["metric"])
+}
+
+func TestSpanSetOperationName(t *testing.T) {
+	span := NewTracer().
+		StartSpan("test").
+		SetOperationName("op")
+	assert.Equal(t, "op", span.(*Span).Name)
+}
